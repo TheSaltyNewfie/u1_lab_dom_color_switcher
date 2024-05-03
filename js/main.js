25c5ee382b4c25459721ca67353c394ac09c5ad9
Tobias Dodge <tobydodge65@gmail.com>
2024-05-03 13:08:09 -0230

    Finished original goal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 // Write your DOM code here!
+const redbtn = document.querySelector('.red')
+const whitebtn = document.querySelector('.white')
+const bluebtn = document.querySelector('.blue')
+const yellowbtn = document.querySelector('.yellow')
+
+redbtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'red'
+})
+
+whitebtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white'
+})
+
+bluebtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'blue'
+})
+
+yellowbtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'yellow'
+})
